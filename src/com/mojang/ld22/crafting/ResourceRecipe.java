8ec45b3492a07cb6ddf30e601790d8bd9217477e
Davideesk <benepe20@gmail.com>
2013-07-17 17:39:37 -0500

    Commented on ResourceRecipe.java & ToolRecipe.java
    2 short classes out of the way.

--- src/com/mojang/ld22/crafting/ResourceRecipe.java
+++ src/com/mojang/ld22/crafting/ResourceRecipe.java
@@ -1,21 +1,20 @@
 package com.mojang.ld22.crafting;
 
 import com.mojang.ld22.entity.Player;
-import com.mojang.ld22.gfx.Color;
-import com.mojang.ld22.gfx.Font;
-import com.mojang.ld22.gfx.Screen;
 import com.mojang.ld22.item.ResourceItem;
 import com.mojang.ld22.item.resource.Resource;
 
 public class ResourceRecipe extends Recipe {
-	private Resource resource;
+	private Resource resource; //The resource used in this recipe
 
+	/** Adds a recipe to craft a resource */
 	public ResourceRecipe(Resource resource) {
-		super(new ResourceItem(resource, 1));
-		this.resource = resource;
+		super(new ResourceItem(resource, 1)); //this goes through Recipe.java to be put on a list.
+		this.resource = resource; //resource to be added
 	}
 
+	/** Adds the resource into your inventory */
 	public void craft(Player player) {
-		player.inventory.add(0, new ResourceItem(resource, 1));
-	}
+		player.inventory.add(0, new ResourceItem(resource, 1)); //adds the resource
+		}
 }
--- src/com/mojang/ld22/crafting/ToolRecipe.java
+++ src/com/mojang/ld22/crafting/ToolRecipe.java
@@ -5,16 +5,26 @@ import com.mojang.ld22.item.ToolItem;
 import com.mojang.ld22.item.ToolType;
 
 public class ToolRecipe extends Recipe {
+	
+	/* Blue text adds description notes to a variable. Hover your mouse over the blue word 'type' or 'level' */
+	
+	/** The type of tool, example: (ToolType.sword, ToolType.axe, ToolType.hoe) */
 	private ToolType type;
+	
+	/** The level of the tool (0 = wood, 1 = rock/stone, 2 = iron, 3 = gold, */
 	private int level;
 
+	/** Adds a recipe to add a tool, input a ToolType and it's level. 
+	 Example: ToolRecipe(ToolType.sword , 2) will create a iron sword.
+	 */
 	public ToolRecipe(ToolType type, int level) {
-		super(new ToolItem(type, level));
-		this.type = type;
-		this.level = level;
+		super(new ToolItem(type, level)); //this goes through Recipe.java to be put on a list.
+		this.type = type; //adds the type
+		this.level = level; //adds the level
 	}
 
+	/** Crafts the tool into your inventory */
 	public void craft(Player player) {
-		player.inventory.add(0, new ToolItem(type, level));
+		player.inventory.add(0, new ToolItem(type, level)); //adds the tool into your inventory
 	}
 }

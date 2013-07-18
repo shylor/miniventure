3b610e6f2a5b0e51a24679380978295b2fed69f4
Davideesk <benepe20@gmail.com>
2013-07-18 18:55:17 -0500

    Commented Tile.java & SaplingTile.java

--- src/com/mojang/ld22/level/tile/SaplingTile.java
+++ src/com/mojang/ld22/level/tile/SaplingTile.java
@@ -6,35 +6,36 @@ import com.mojang.ld22.gfx.Screen;
 import com.mojang.ld22.level.Level;
 
 public class SaplingTile extends Tile {
-	private Tile onType;
-	private Tile growsTo;
+	private Tile onType; // The tile it grows on (Grass/Sand)
+	private Tile growsTo; // What the sapling grows into (Tree/Cactus)
 
 	public SaplingTile(int id, Tile onType, Tile growsTo) {
-		super(id);
-		this.onType = onType;
-		this.growsTo = growsTo;
-		connectsToSand = onType.connectsToSand;
-		connectsToGrass = onType.connectsToGrass;
-		connectsToWater = onType.connectsToWater;
-		connectsToLava = onType.connectsToLava;
+		super(id); // Assigns the id
+		this.onType = onType; // Assigns the tile it grows on
+		this.growsTo = growsTo; // Assigns the tile it grows into
+		connectsToSand = onType.connectsToSand; //Becomes connect to sand if the type it grows on can connect to sand.
+		connectsToGrass = onType.connectsToGrass; //Becomes connect to grass if the type it grows on can connect to grass.
+		connectsToWater = onType.connectsToWater; //Becomes connect to water if the type it grows on can connect to water.
+		connectsToLava = onType.connectsToLava; //Becomes connect to lava if the type it grows on can connect to sand.
 	}
 
 	public void render(Screen screen, Level level, int x, int y) {
-		onType.render(screen, level, x, y);
-		int col = Color.get(10, 40, 50, -1);
-		screen.render(x * 16 + 4, y * 16 + 4, 11 + 3 * 32, col, 0);
+		onType.render(screen, level, x, y); // Calls the render method of the tile it grows on
+		int col = Color.get(10, 40, 50, -1); // Color of the sapling
+		screen.render(x * 16 + 4, y * 16 + 4, 11 + 3 * 32, col, 0); // renders the small sprite of the sapling
 	}
 
+
 	public void tick(Level level, int x, int y) {
-		int age = level.getData(x, y) + 1;
+		int age = level.getData(x, y) + 1; // Gets the data of the age of the sapling
 		if (age > 100) {
-			level.setTile(x, y, growsTo, 0);
+			level.setTile(x, y, growsTo, 0); //If that age number is above 100 then it grows to the 'growsTo' tile.
 		} else {
-			level.setData(x, y, age);
+			level.setData(x, y, age); // If it is equal or smaller than 100, then it will add 1 to the age
 		}
 	}
 
 	public void hurt(Level level, int x, int y, Mob source, int dmg, int attackDir) {
-		level.setTile(x, y, onType, 0);
+		level.setTile(x, y, onType, 0); //If the sapling is hit, then it will turn back (disappear) into the block it grew on.
 	}
 }
--- src/com/mojang/ld22/level/tile/Tile.java
+++ src/com/mojang/ld22/level/tile/Tile.java
@@ -11,78 +11,89 @@ import com.mojang.ld22.item.resource.Resource;
 import com.mojang.ld22.level.Level;
 
 public class Tile {
-	public static int tickCount = 0;
-	protected Random random = new Random();
+	public static int tickCount = 0; //A global tickCount used in the Lava & water tiles.
+	protected Random random = new Random(); // Random is used for random numbers (duh!).
 
-	public static Tile[] tiles = new Tile[256];
-	public static Tile grass = new GrassTile(0);
-	public static Tile rock = new RockTile(1);
-	public static Tile water = new WaterTile(2);
-	public static Tile flower = new FlowerTile(3);
-	public static Tile tree = new TreeTile(4);
-	public static Tile dirt = new DirtTile(5);
-	public static Tile sand = new SandTile(6);
-	public static Tile cactus = new CactusTile(7);
+	public static Tile[] tiles = new Tile[256]; // An array of tiles
+	public static Tile grass = new GrassTile(0); // creates a grass tile with the Id of 0, (I don't need to explain the other simple ones)
+	public static Tile rock = new RockTile(1); 
+	public static Tile water = new WaterTile(2); 
+	public static Tile flower = new FlowerTile(3); 
+	public static Tile tree = new TreeTile(4); 
+	public static Tile dirt = new DirtTile(5); 
+	public static Tile sand = new SandTile(6); 
+	public static Tile cactus = new CactusTile(7); 
 	public static Tile hole = new HoleTile(8);
-	public static Tile treeSapling = new SaplingTile(9, grass, tree);
-	public static Tile cactusSapling = new SaplingTile(10, sand, cactus);
-	public static Tile farmland = new FarmTile(11);
-	public static Tile wheat = new WheatTile(12);
+	public static Tile treeSapling = new SaplingTile(9, grass, tree); // tree sapling; plant on grass, eventually becomes a tree
+	public static Tile cactusSapling = new SaplingTile(10, sand, cactus); // cactus sapling; plant on sand, eventually becomes a cactus
+	public static Tile farmland = new FarmTile(11); // farmland (tilled dirt)
+	public static Tile wheat = new WheatTile(12); //wheat (planted dirt)
 	public static Tile lava = new LavaTile(13);
-	public static Tile stairsDown = new StairsTile(14, false);
-	public static Tile stairsUp = new StairsTile(15, true);
-	public static Tile infiniteFall = new InfiniteFallTile(16);
+	public static Tile stairsDown = new StairsTile(14, false); // Stairs leading down
+	public static Tile stairsUp = new StairsTile(15, true); // Stairs leading up
+	public static Tile infiniteFall = new InfiniteFallTile(16); // Air tile in the sky.
 	public static Tile cloud = new CloudTile(17);
 	public static Tile hardRock = new HardRockTile(18);
-	public static Tile ironOre = new OreTile(19, Resource.ironOre);
+	/* Ores */
+	public static Tile ironOre = new OreTile(19, Resource.ironOre); 
 	public static Tile goldOre = new OreTile(20, Resource.goldOre);
 	public static Tile gemOre = new OreTile(21, Resource.gem);
 	public static Tile cloudCactus = new CloudCactusTile(22);
 
-	public final byte id;
+	public final byte id; // Id of the tile
 
-	public boolean connectsToGrass = false;
-	public boolean connectsToSand = false;
-	public boolean connectsToLava = false;
-	public boolean connectsToWater = false;
+	public boolean connectsToGrass = false; // sees if the tile connects to grass
+	public boolean connectsToSand = false; // sees if the tile connects to sand
+	public boolean connectsToLava = false; // sees if the tile connects to lava
+	public boolean connectsToWater = false; // sees if the tile connects to water
 
 	public Tile(int id) {
-		this.id = (byte) id;
-		if (tiles[id] != null) throw new RuntimeException("Duplicate tile ids!");
-		tiles[id] = this;
+		this.id = (byte) id; // creates the id
+		if (tiles[id] != null) throw new RuntimeException("Duplicate tile ids!"); // You cannot have over-lapping ids
+		tiles[id] = this; // Assigns the id
 	}
 
+	/** Render method, used in sub-classes */
 	public void render(Screen screen, Level level, int x, int y) {
 	}
 
+	/** Returns if the player can walk on it, overrides in sub-classes  */
 	public boolean mayPass(Level level, int x, int y, Entity e) {
 		return true;
 	}
 
+	/** Gets the light radius of a tile, Bigger number = bigger circle */
 	public int getLightRadius(Level level, int x, int y) {
 		return 0;
 	}
 
+	/** What happens when you hit the tile (ex: punching a tree) */
 	public void hurt(Level level, int x, int y, Mob source, int dmg, int attackDir) {
 	}
 
+	/** What happens when you run into the tile (ex: run into a cactus) */
 	public void bumpedInto(Level level, int xt, int yt, Entity entity) {
 	}
 
+	/** Update method */
 	public void tick(Level level, int xt, int yt) {
 	}
 
+	/** What happens when you are inside the tile (ex: lava) */
 	public void steppedOn(Level level, int xt, int yt, Entity entity) {
 	}
 
+	/** What happens when you hit an item on a tile (ex: Pickaxe on rock) */
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
 		return false;
 	}
 
+	/** Useless method? Doesn't seem to be used in the tiles */
 	public boolean use(Level level, int xt, int yt, Player player, int attackDir) {
 		return false;
 	}
 
+	/** Sees if the tile connects to Water or Lava. */
 	public boolean connectsToLiquid() {
 		return connectsToWater || connectsToLava;
 	}

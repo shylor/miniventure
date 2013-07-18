2ba7735710f011b2a1b40253909fce20b434cd65
Davideesk <benepe20@gmail.com>
2013-07-18 17:27:43 -0500

    Commented Menu.java & AboutMenu.java

--- src/com/mojang/ld22/screen/AboutMenu.java
+++ src/com/mojang/ld22/screen/AboutMenu.java
@@ -5,28 +5,33 @@ import com.mojang.ld22.gfx.Font;
 import com.mojang.ld22.gfx.Screen;
 
 public class AboutMenu extends Menu {
-	private Menu parent;
+	private Menu parent; // Creates a parent object to go back to
 
+	/** The about menu is a read menu about what the game was made for. Only contains text and a black background */
 	public AboutMenu(Menu parent) {
-		this.parent = parent;
+		this.parent = parent; // The parent Menu that it will go back to.
 	}
 
+	/** The update method. 60 updates per second. */
 	public void tick() {
 		if (input.attack.clicked || input.menu.clicked) {
-			game.setMenu(parent);
+			game.setMenu(parent); // If the user presses the "Attack" or "Menu" button, it will go back to the parent menu.
 		}
 	}
 
+	/** Renders the text on the screen */
 	public void render(Screen screen) {
-		screen.clear(0);
+		screen.clear(0); // clears the screen to be a black color.
 
-		Font.draw("About Minicraft", screen, 2 * 8 + 4, 1 * 8, Color.get(0, 555, 555, 555));
-		Font.draw("Minicraft was made", screen, 0 * 8 + 4, 3 * 8, Color.get(0, 333, 333, 333));
-		Font.draw("by Markus Persson", screen, 0 * 8 + 4, 4 * 8, Color.get(0, 333, 333, 333));
-		Font.draw("For the 22'nd ludum", screen, 0 * 8 + 4, 5 * 8, Color.get(0, 333, 333, 333));
-		Font.draw("dare competition in", screen, 0 * 8 + 4, 6 * 8, Color.get(0, 333, 333, 333));
-		Font.draw("december 2011.", screen, 0 * 8 + 4, 7 * 8, Color.get(0, 333, 333, 333));
-		Font.draw("it is dedicated to", screen, 0 * 8 + 4, 9 * 8, Color.get(0, 333, 333, 333));
-		Font.draw("my father. <3", screen, 0 * 8 + 4, 10 * 8, Color.get(0, 333, 333, 333));
+		/* Font.draw Parameters: Font.draw(String text, Screen screen, int x, int y, int color) */
+		
+		Font.draw("About Minicraft", screen, 2 * 8 + 4, 1 * 8, Color.get(0, 555, 555, 555)); //draws Title text
+		Font.draw("Minicraft was made", screen, 0 * 8 + 4, 3 * 8, Color.get(0, 333, 333, 333)); // draws text
+		Font.draw("by Markus Persson", screen, 0 * 8 + 4, 4 * 8, Color.get(0, 333, 333, 333)); // draws text
+		Font.draw("For the 22'nd ludum", screen, 0 * 8 + 4, 5 * 8, Color.get(0, 333, 333, 333)); // draws text
+		Font.draw("dare competition in", screen, 0 * 8 + 4, 6 * 8, Color.get(0, 333, 333, 333)); // draws text
+		Font.draw("december 2011.", screen, 0 * 8 + 4, 7 * 8, Color.get(0, 333, 333, 333)); // draws text
+		Font.draw("it is dedicated to", screen, 0 * 8 + 4, 9 * 8, Color.get(0, 333, 333, 333)); // draws text
+		Font.draw("my father. <3", screen, 0 * 8 + 4, 10 * 8, Color.get(0, 333, 333, 333)); // draws text
 	}
 }
--- src/com/mojang/ld22/screen/Menu.java
+++ src/com/mojang/ld22/screen/Menu.java
@@ -9,43 +9,47 @@ import com.mojang.ld22.gfx.Font;
 import com.mojang.ld22.gfx.Screen;
 
 public class Menu {
-	protected Game game;
-	protected InputHandler input;
+	protected Game game; // game object used in the menu classes.
+	protected InputHandler input; // input object used in the menu classes.
 
+	/** Initialization step, adds the game & input objects. */
 	public void init(Game game, InputHandler input) {
 		this.input = input;
 		this.game = game;
 	}
 
+	/** update method used in menus */
 	public void tick() {
 	}
 
+	/** render method used in menus */
 	public void render(Screen screen) {
 	}
 
+	/** renders items in a list, used for the inventory, crafting, and chest menus */
 	public void renderItemList(Screen screen, int xo, int yo, int x1, int y1, List<? extends ListItem> listItems, int selected) {
-		boolean renderCursor = true;
+		boolean renderCursor = true;// Renders the ">" "<" arrows between a name.
 		if (selected < 0) {
-			selected = -selected - 1;
-			renderCursor = false;
+			selected = -selected - 1; // If the selected is smaller than 0, then it will add one.
+			renderCursor = false; // doesn't render the arrows between the name
 		}
-		int w = x1 - xo;
-		int h = y1 - yo - 1;
-		int i0 = 0;
-		int i1 = listItems.size();
-		if (i1 > h) i1 = h;
-		int io = selected - h / 2;
-		if (io > listItems.size() - h) io = listItems.size() - h;
-		if (io < 0) io = 0;
+		int w = x1 - xo; // Width variable determined by given values
+		int h = y1 - yo - 1; // Height variable determined by given values
+ 		int i0 = 0; // Beginning variable of the list loop
+		int i1 = listItems.size(); // End variable of the list loop
+		if (i1 > h) i1 = h; // If the end variable is larger than the height variable, then end variable will equal height variable.
+		int io = selected - h / 2; // Middle of the list, (selected item). For scrolling effect
+		if (io > listItems.size() - h) io = listItems.size() - h; //if the middle is coming near the bottom, then the selected will change.
+		if (io < 0) io = 0; // if the middle is coming near the top, then the selected will change
 
 		for (int i = i0; i < i1; i++) {
-			listItems.get(i + io).renderInventory(screen, (1 + xo) * 8, (i + 1 + yo) * 8);
+			listItems.get(i + io).renderInventory(screen, (1 + xo) * 8, (i + 1 + yo) * 8); // this will render all the items in the inventory
 		}
 
 		if (renderCursor) {
-			int yy = selected + 1 - io + yo;
-			Font.draw(">", screen, (xo + 0) * 8, yy * 8, Color.get(5, 555, 555, 555));
-			Font.draw("<", screen, (xo + w) * 8, yy * 8, Color.get(5, 555, 555, 555));
+			int yy = selected + 1 - io + yo; // the y position of the currently selected item
+			Font.draw(">", screen, (xo + 0) * 8, yy * 8, Color.get(5, 555, 555, 555)); // draws the left cursor next to the name
+			Font.draw("<", screen, (xo + w) * 8, yy * 8, Color.get(5, 555, 555, 555)); // draws the right cursor next to the name
 		}
 	}
 }

3cab43038ceea61205c460cfbf7400eec2c68bd3
Davideesk <benepe20@gmail.com>
2013-07-19 17:29:10 -0500

    Commented ContainerMenu.java & CraftingMenu.java

--- src/com/mojang/ld22/screen/ContainerMenu.java
+++ src/com/mojang/ld22/screen/ContainerMenu.java
@@ -6,62 +6,77 @@ import com.mojang.ld22.gfx.Font;
 import com.mojang.ld22.gfx.Screen;
 
 public class ContainerMenu extends Menu {
-	private Player player;
-	private Inventory container;
-	private int selected = 0;
-	private String title;
-	private int oSelected;
-	private int window = 0;
+	private Player player; // The player that is looking inside the chest
+	private Inventory container; // The inventory of the chest
+	private int selected = 0; // The selected item
+	private String title; // The title of the chest
+	private int oSelected; // the old selected option (this is used to temporarily save spots moving from chest to inventory & vice-versa)
+	private int window = 0; // currently selected window (player's inventory, or chest's inventory)
 
+	/** The container menu class is used for chests */
 	public ContainerMenu(Player player, String title, Inventory container) {
-		this.player = player;
-		this.title = title;
-		this.container = container;
+		this.player = player; // Player looking inside the chest
+		this.title = title; // Title of the chest
+		this.container = container; // Inventory of the chest
 	}
 
 	public void tick() {
-		if (input.menu.clicked) game.setMenu(null);
+		if (input.menu.clicked) game.setMenu(null); // If the player selects the "menu" key, then it will exit the chest
 
-		if (input.left.clicked) {
-			window = 0;
-			int tmp = selected;
-			selected = oSelected;
-			oSelected = tmp;
+		if (input.left.clicked) { //if the left key is pressed...
+			window = 0; // The current window will be of the chest
+			int tmp = selected; // temp integer will be the currently selected
+			selected = oSelected; // selected will become the oSelected
+			oSelected = tmp; // oSelected will become the temp integer (save spot for when you switch)
 		}
-		if (input.right.clicked) {
-			window = 1;
-			int tmp = selected;
-			selected = oSelected;
-			oSelected = tmp;
+		if (input.right.clicked) { //if the right key is pressed...
+			window = 1; // The current window will be of the player's inventory
+			int tmp = selected; // temp integer will be the currently selected
+			selected = oSelected; // selected will become the oSelected
+			oSelected = tmp; // oSelected will become the temp integer (save spot for when you switch)
 		}
 
-		Inventory i = window == 1 ? player.inventory : container;
-		Inventory i2 = window == 0 ? player.inventory : container;
+		/* Below is an example of the "ternary operator"  
+		  
+		  which is read like (example): 
+		  'boolean statement ? true result : false result;'
+		  
+		  It's the exact same thing as:
+		  
+		  if (boolean statement) {
+    		true result;
+			} else {
+    		false result;
+		  }
+		  
+		  It's just for convince sake, Google " ternary operator " for more info
+		  
+		 * */
+		
+		Inventory i = window == 1 ? player.inventory : container; // If the window is equal to 1, then the main inventory is the player inventory, else it's the chest's
+		Inventory i2 = window == 0 ? player.inventory : container; // If the window is equal to 0, then the backup inventory is the player inventory, else it's the chest's
 
-		int len = i.items.size();
-		if (selected < 0) selected = 0;
-		if (selected >= len) selected = len - 1;
+		int len = i.items.size(); // Size of the main inventory
+		if (input.up.clicked) selected--; // If the up key is press then the selection will go up one item
+		if (input.down.clicked) selected++; // If the down key is pressed then the selection will go down one item
 
-		if (input.up.clicked) selected--;
-		if (input.down.clicked) selected++;
+		if (len == 0) selected = 0; // If the size of the inventory is 0, then it will stay selected at 0
+		if (selected < 0) selected += len; // If the current selection is less than 0 (first entry) than it will loop to the bottom.
+		if (selected >= len) selected -= len; // If the current selection goes past the bottom entry, then it will loop to the top.
 
-		if (len == 0) selected = 0;
-		if (selected < 0) selected += len;
-		if (selected >= len) selected -= len;
-
-		if (input.attack.clicked && len > 0) {
-			i2.add(oSelected, i.items.remove(selected));
-			if (selected >= i.items.size()) selected = i.items.size() - 1;
+		if (input.attack.clicked && len > 0) { // If the "Attack" key is pressed and the inventory's size is bigger than 0...
+			i2.add(oSelected, i.items.remove(selected)); // It will add the item to the new inventory, and remove it from the old one.
+			if (selected >= i.items.size()) selected = i.items.size() - 1; // This fixes the selected item to the latest one.
 		}
 	}
 
 	public void render(Screen screen) {
-		if (window == 1) screen.setOffset(6 * 8, 0);
-		Font.renderFrame(screen, title, 1, 1, 12, 11);
-		renderItemList(screen, 1, 1, 12, 11, container.items, window == 0 ? selected : -oSelected - 1);
+		if (window == 1) screen.setOffset(6 * 8, 0); // Offsets the windows for when the player's inventory is selected
+		Font.renderFrame(screen, title, 1, 1, 12, 11); // Renders the chest's window
+		renderItemList(screen, 1, 1, 12, 11, container.items, window == 0 ? selected : -oSelected - 1); // renders all the items from the chest's inventory
 
-		Font.renderFrame(screen, "inventory", 13, 1, 13 + 11, 11);
-		renderItemList(screen, 13, 1, 13 + 11, 11, player.inventory.items, window == 1 ? selected : -oSelected - 1);
-		screen.setOffset(0, 0);
+		Font.renderFrame(screen, "inventory", 13, 1, 13 + 11, 11); // renders the player's inventory
+		renderItemList(screen, 13, 1, 13 + 11, 11, player.inventory.items, window == 1 ? selected : -oSelected - 1); // renders all the items from the player's inventory
+		screen.setOffset(0, 0); // Fixes the offset back to normal
 	}
 }
--- src/com/mojang/ld22/screen/CraftingMenu.java
+++ src/com/mojang/ld22/screen/CraftingMenu.java
@@ -15,84 +15,85 @@ import com.mojang.ld22.item.ResourceItem;
 import com.mojang.ld22.sound.Sound;
 
 public class CraftingMenu extends Menu {
-	private Player player;
-	private int selected = 0;
+	private Player player; // the player that opened this menu
+	private int selected = 0; // current selected item
 
-	private List<Recipe> recipes;
+	private List<Recipe> recipes; // List of recipes used in this menu (workbench, anvil, oven, etc)
 
 	public CraftingMenu(List<Recipe> recipes, Player player) {
-		this.recipes = new ArrayList<Recipe>(recipes);
-		this.player = player;
+		this.recipes = new ArrayList<Recipe>(recipes); // Assigns the recipes
+		this.player = player; // the player
 
 		for (int i = 0; i < recipes.size(); i++) {
-			this.recipes.get(i).checkCanCraft(player);
+			this.recipes.get(i).checkCanCraft(player); // Checks if the player can craft the item(s)
 		}
 
+		/* This sorts the recipes so that the ones you can craft will appear on top */
 		Collections.sort(this.recipes, new Comparator<Recipe>() {
 			public int compare(Recipe r1, Recipe r2) {
-				if (r1.canCraft && !r2.canCraft) return -1;
-				if (!r1.canCraft && r2.canCraft) return 1;
-				return 0;
+				if (r1.canCraft && !r2.canCraft) return -1; // if the first item can be crafted while the second can't, the first one will go above in the list
+				if (!r1.canCraft && r2.canCraft) return 1; // if the second item can be crafted while the first can't, the second will go over that one.
+				return 0; // else don't change position
 			}
 		});
 	}
 
 	public void tick() {
-		if (input.menu.clicked) game.setMenu(null);
+		if (input.menu.clicked) game.setMenu(null); // if the player presses the "Menu" key then it will exit this menu
 
-		if (input.up.clicked) selected--;
-		if (input.down.clicked) selected++;
+		if (input.up.clicked) selected--; // If the up key is press then the selection will go up one item
+		if (input.down.clicked) selected++; // If the down key is pressed then the selection will go down one item
 
-		int len = recipes.size();
-		if (len == 0) selected = 0;
-		if (selected < 0) selected += len;
-		if (selected >= len) selected -= len;
+		int len = recipes.size(); // Size of the recipe list
+		if (len == 0) selected = 0; // If the size of the list is 0, then it will stay selected at 0
+		if (selected < 0) selected += len; // If the current selection is less than 0 (first entry) than it will loop to the bottom.
+		if (selected >= len) selected -= len; // If the current selection goes past the bottom entry, then it will loop to the top.
 
-		if (input.attack.clicked && len > 0) {
-			Recipe r = recipes.get(selected);
-			r.checkCanCraft(player);
-			if (r.canCraft) {
-				r.deductCost(player);
-				r.craft(player);
-				Sound.craft.play();
+		if (input.attack.clicked && len > 0) {// If the "Attack" key is pressed and the recipe's size is bigger than 0...
+			Recipe r = recipes.get(selected); // The current recipe selected
+			r.checkCanCraft(player); // Checks if the player can craft this recipe
+			if (r.canCraft) { // If the player can craft the item...
+				r.deductCost(player); // Then it will deduct the items from the player's inventory
+				r.craft(player); // It will craft (add) the item into the player's inventory
+				Sound.craft.play(); // Plays a sound to tell you that a item has been crafted
 			}
-			for (int i = 0; i < recipes.size(); i++) {
-				recipes.get(i).checkCanCraft(player);
+			for (int i = 0; i < recipes.size(); i++) { // Loops through all the recipes in the list
+				recipes.get(i).checkCanCraft(player); // Refreshes the recipe list if the player can now craft a new item.
 			}
 		}
 	}
 
 	public void render(Screen screen) {
-		Font.renderFrame(screen, "Have", 12, 1, 19, 3);
-		Font.renderFrame(screen, "Cost", 12, 4, 19, 11);
-		Font.renderFrame(screen, "Crafting", 0, 1, 11, 11);
-		renderItemList(screen, 0, 1, 11, 11, recipes, selected);
+		Font.renderFrame(screen, "Have", 12, 1, 19, 3); // renders the 'have' items window
+		Font.renderFrame(screen, "Cost", 12, 4, 19, 11); // renders the 'cost' items window
+		Font.renderFrame(screen, "Crafting", 0, 1, 11, 11); // renders the main crafting window
+		//renderItemList(screen, 0, 1, 11, 11, recipes, selected); // renders all the items in the recipe list
 
-		if (recipes.size() > 0) {
-			Recipe recipe = recipes.get(selected);
-			int hasResultItems = player.inventory.count(recipe.resultTemplate);
-			int xo = 13 * 8;
-			screen.render(xo, 2 * 8, recipe.resultTemplate.getSprite(), recipe.resultTemplate.getColor(), 0);
-			Font.draw("" + hasResultItems, screen, xo + 8, 2 * 8, Color.get(-1, 555, 555, 555));
+		if (recipes.size() > 0) { // If the size of the recipe list is above 0...
+			Recipe recipe = recipes.get(selected); // currently selected recipe
+			int hasResultItems = player.inventory.count(recipe.resultTemplate); // Counts the number of items to see if you can craft the recipe
+			int xo = 13 * 8; // x coordinate of the items in the 'have' and 'cost' windows
+			screen.render(xo, 2 * 8, recipe.resultTemplate.getSprite(), recipe.resultTemplate.getColor(), 0); // Renders the sprites in the 'have' & 'cost' windows
+			Font.draw("" + hasResultItems, screen, xo + 8, 2 * 8, Color.get(-1, 555, 555, 555)); // draws the amount in the 'have' menu
 
-			List<Item> costs = recipe.costs;
-			for (int i = 0; i < costs.size(); i++) {
-				Item item = costs.get(i);
-				int yo = (5 + i) * 8;
-				screen.render(xo, yo, item.getSprite(), item.getColor(), 0);
-				int requiredAmt = 1;
-				if (item instanceof ResourceItem) {
-					requiredAmt = ((ResourceItem) item).count;
+			List<Item> costs = recipe.costs; // the list items that is needed to make the recipe
+			for (int i = 0; i < costs.size(); i++) { // Loops through the costs list
+				Item item = costs.get(i); // Current cost item
+				int yo = (5 + i) * 8; // y coordinate of the cost item
+				screen.render(xo, yo, item.getSprite(), item.getColor(), 0); // renders the cost item
+				int requiredAmt = 1; // required amount need to craft (normally 1)
+				if (item instanceof ResourceItem) { // If the item is a resource...
+					requiredAmt = ((ResourceItem) item).count; // get's the amount needed to craft the item
 				}
-				int has = player.inventory.count(item);
-				int color = Color.get(-1, 555, 555, 555);
-				if (has < requiredAmt) {
-					color = Color.get(-1, 222, 222, 222);
+				int has = player.inventory.count(item); // This is the amount of the resource you have in your inventory
+				int color = Color.get(-1, 555, 555, 555); // color in the 'cost' window
+				if (has < requiredAmt) { // If the player has less than required of the resource
+					color = Color.get(-1, 222, 222, 222); // then change the color to gray.
 				}
-				if (has > 99) has = 99;
-				Font.draw("" + requiredAmt + "/" + has, screen, xo + 8, yo, color);
+				if (has > 99) has = 99; // if the player has over 99 of the resource, then just display 99 (for space)
+				Font.draw("" + requiredAmt + "/" + has, screen, xo + 8, yo, color); // Draw "#required/#has" text next to the icon 
 			}
 		}
-		// renderItemList(screen, 12, 4, 19, 11, recipes.get(selected).costs, -1);
+		
 	}
 }

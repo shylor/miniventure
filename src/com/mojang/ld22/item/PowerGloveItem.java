52f003de1023f5e55f2cfeca2a1b93ad9d2187af
Davideesk <benepe20@gmail.com>
2013-07-19 16:00:32 -0500

    Finished PowerGloveItem.java

--- src/com/mojang/ld22/item/PowerGloveItem.java
+++ src/com/mojang/ld22/item/PowerGloveItem.java
@@ -8,6 +8,7 @@ import com.mojang.ld22.gfx.Font;
 import com.mojang.ld22.gfx.Screen;
 
 public class PowerGloveItem extends Item {
+	
 	public int getColor() {
 		return Color.get(-1, 100, 320, 430); //sets the color of the powerglove
 	}
@@ -17,12 +18,12 @@ public class PowerGloveItem extends Item {
 	}
 
 	public void renderIcon(Screen screen, int x, int y) {
-		screen.render(x, y, getSprite(), getColor(), 0);
+		screen.render(x, y, getSprite(), getColor(), 0); // Renders the icon of the power glove to the screen
 	}
 
 	public void renderInventory(Screen screen, int x, int y) {
-		screen.render(x, y, getSprite(), getColor(), 0);
-		Font.draw(getName(), screen, x + 8, y, Color.get(-1, 555, 555, 555));
+		screen.render(x, y, getSprite(), getColor(), 0); // renders the icon of the power glove to the screen
+		Font.draw(getName(), screen, x + 8, y, Color.get(-1, 555, 555, 555)); // renders the name of the powerglove to the screen
 	}
 
 	public String getName() {
@@ -30,11 +31,11 @@ public class PowerGloveItem extends Item {
 	}
 
 	public boolean interact(Player player, Entity entity, int attackDir) {
-		if (entity instanceof Furniture) {
-			Furniture f = (Furniture) entity;
-			f.take(player);
-			return true;
+		if (entity instanceof Furniture) { // If the power glove hits a furnace
+			Furniture f = (Furniture) entity; // Assigns the furniture
+			f.take(player); // Takes (picks up) the furniture
+			return true; // Method returns true
 		}
-		return false;
+		return false; // method returns false if it did not hit a furniture entity.
 	}
 }

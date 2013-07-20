e7288900defeaa06ae88f2e406814cc731fc3bec
Davideesk <benepe20@gmail.com>
2013-07-19 19:31:09 -0500

    Commented FurnitureItem.java & ResourceItem.java

--- src/com/mojang/ld22/item/FurnitureItem.java
+++ src/com/mojang/ld22/item/FurnitureItem.java
@@ -10,52 +10,61 @@ import com.mojang.ld22.level.Level;
 import com.mojang.ld22.level.tile.Tile;
 
 public class FurnitureItem extends Item {
-	public Furniture furniture;
-	public boolean placed = false;
+	public Furniture furniture; // the furniture of this item
+	public boolean placed = false; // value if the furniture has been placed or not.
 
 	public FurnitureItem(Furniture furniture) {
-		this.furniture = furniture;
+		this.furniture = furniture; // Assigns the furniture to the item
 	}
 
+	/** Gets the color of the furniture */
 	public int getColor() {
 		return furniture.col;
 	}
 
+	/** Gets the sprite of the furniture */
 	public int getSprite() {
 		return furniture.sprite + 10 * 32;
 	}
 
+	/** Renders the icon used for the furniture */
 	public void renderIcon(Screen screen, int x, int y) {
-		screen.render(x, y, getSprite(), getColor(), 0);
+		screen.render(x, y, getSprite(), getColor(), 0); // renders the icon
 	}
 
+	/** Renders the icon, and name of the furniture */
 	public void renderInventory(Screen screen, int x, int y) {
-		screen.render(x, y, getSprite(), getColor(), 0);
-		Font.draw(furniture.name, screen, x + 8, y, Color.get(-1, 555, 555, 555));
+		screen.render(x, y, getSprite(), getColor(), 0); // renders the icon
+		Font.draw(furniture.name, screen, x + 8, y, Color.get(-1, 555, 555, 555)); // draws the name of the furniture
 	}
 
+	/** What happens when you pick up the item off the ground (Not with the power glove) */
 	public void onTake(ItemEntity itemEntity) {
 	}
 
+	/** Determines if you can attack enemies with furniture (you can't) */
 	public boolean canAttack() {
 		return false;
 	}
 
+	/** What happens when you press the "Attack" key with the furniture in your hands */
 	public boolean interactOn(Tile tile, Level level, int xt, int yt, Player player, int attackDir) {
-		if (tile.mayPass(level, xt, yt, furniture)) {
-			furniture.x = xt * 16 + 8;
-			furniture.y = yt * 16 + 8;
-			level.add(furniture);
-			placed = true;
+		if (tile.mayPass(level, xt, yt, furniture)) { // If the furniture can go on the tile
+			furniture.x = xt * 16 + 8; // Placed furniture's X position
+			furniture.y = yt * 16 + 8; // Placed furniture's Y position
+			level.add(furniture); // adds the furniture to the world
+			placed = true; // the value becomes true, which removes it from the player's active item
 			return true;
 		}
 		return false;
 	}
 
+	/** Removes this item from the player's active item slot when depleted is true */
 	public boolean isDepleted() {
 		return placed;
 	}
 	
+	/** Gets the name of the furniture */
 	public String getName() {
 		return furniture.name;
 	}
--- src/com/mojang/ld22/item/ResourceItem.java
+++ src/com/mojang/ld22/item/ResourceItem.java
@@ -10,53 +10,61 @@ import com.mojang.ld22.level.Level;
 import com.mojang.ld22.level.tile.Tile;
 
 public class ResourceItem extends Item {
-	public Resource resource;
-	public int count = 1;
+	public Resource resource; // The resource of this item
+	public int count = 1; // The amount of resources
 
 	public ResourceItem(Resource resource) {
-		this.resource = resource;
+		this.resource = resource; //assigns the resource
 	}
 
 	public ResourceItem(Resource resource, int count) {
-		this.resource = resource;
-		this.count = count;
+		this.resource = resource; //assigns the resource
+		this.count = count; //assigns the count
 	}
 
+	/** Gets the color of the resource */
 	public int getColor() {
 		return resource.color;
 	}
-
+	
+	/** Gets the sprite of the resource */
 	public int getSprite() {
 		return resource.sprite;
 	}
 
+	/** Renders the icon used for the resource */
 	public void renderIcon(Screen screen, int x, int y) {
-		screen.render(x, y, resource.sprite, resource.color, 0);
+		screen.render(x, y, resource.sprite, resource.color, 0); // renders the icon
 	}
 
+	/** Renders the icon, name, and count of the resource */
 	public void renderInventory(Screen screen, int x, int y) {
-		screen.render(x, y, resource.sprite, resource.color, 0);
-		Font.draw(resource.name, screen, x + 32, y, Color.get(-1, 555, 555, 555));
-		int cc = count;
-		if (cc > 999) cc = 999;
-		Font.draw("" + cc, screen, x + 8, y, Color.get(-1, 444, 444, 444));
+		screen.render(x, y, resource.sprite, resource.color, 0); // renders the icon
+		Font.draw(resource.name, screen, x + 32, y, Color.get(-1, 555, 555, 555)); // draws the name of the resource
+		int cc = count; // count of the resource
+		if (cc > 999) cc = 999; // If the resource count is above 999, then just render 999 (for spacing reasons)
+		Font.draw("" + cc, screen, x + 8, y, Color.get(-1, 444, 444, 444));// draws the resource count
 	}
 
+	/** Gets the name of the resource */
 	public String getName() {
 		return resource.name;
 	}
 
+	/** What happens when you pick up the item off the ground */
 	public void onTake(ItemEntity itemEntity) {
 	}
 
+	/** What happens when you interact and item with the world */
 	public boolean interactOn(Tile tile, Level level, int xt, int yt, Player player, int attackDir) {
-		if (resource.interactOn(tile, level, xt, yt, player, attackDir)) {
-			count--;
+		if (resource.interactOn(tile, level, xt, yt, player, attackDir)) { // Calls the resource's 'interactOn()' method, if true then...
+			count--; // minuses the count by 1.
 			return true;
 		}
 		return false;
 	}
 
+	/** If the count is equal to, or less than 0. Then this will return true. */
 	public boolean isDepleted() {
 		return count <= 0;
 	}

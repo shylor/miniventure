bf395a991d6a97cbf2ecaf59ffe9883a541b6bb4
Davideesk <benepe20@gmail.com>
2013-07-19 20:27:26 -0500

    Commented on CloudTile.java & DirtTile.java
    Also made a edit to CloudCactusTile.java

--- src/com/mojang/ld22/level/tile/CloudTile.java
+++ src/com/mojang/ld22/level/tile/CloudTile.java
@@ -14,68 +14,75 @@ import com.mojang.ld22.level.Level;
 
 public class CloudTile extends Tile {
 	public CloudTile(int id) {
-		super(id);
+		super(id);//assigns the id
 	}
+	
+	/* Oh boy, it's one of these more complicated connecting tiles classes. 
+	 * 
+	 * Sorry if I can't explain these well - David.
+     */
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int col = Color.get(444, 444, 555, 555);
-		int transitionColor = Color.get(333, 444, 555, -1);
+		int col = Color.get(444, 444, 555, 555); // Color of the cloud
+		int transitionColor = Color.get(333, 444, 555, -1); //Transitional color between connections
 
-		boolean u = level.getTile(x, y - 1) == Tile.infiniteFall;
-		boolean d = level.getTile(x, y + 1) == Tile.infiniteFall;
-		boolean l = level.getTile(x - 1, y) == Tile.infiniteFall;
-		boolean r = level.getTile(x + 1, y) == Tile.infiniteFall;
+		boolean u = level.getTile(x, y - 1) == Tile.infiniteFall; //Checks if the tile above it is a infiniteFall tile.
+		boolean d = level.getTile(x, y + 1) == Tile.infiniteFall; //Checks if the tile below it is a infiniteFall tile.
+		boolean l = level.getTile(x - 1, y) == Tile.infiniteFall; //Checks if the tile to the left is a infiniteFall tile.
+		boolean r = level.getTile(x + 1, y) == Tile.infiniteFall; //Checks if the tile to the right is a infiniteFall tile.
 
-		boolean ul = level.getTile(x - 1, y - 1) == Tile.infiniteFall;
-		boolean dl = level.getTile(x - 1, y + 1) == Tile.infiniteFall;
-		boolean ur = level.getTile(x + 1, y - 1) == Tile.infiniteFall;
-		boolean dr = level.getTile(x + 1, y + 1) == Tile.infiniteFall;
+		boolean ul = level.getTile(x - 1, y - 1) == Tile.infiniteFall; //Checks if the upper-left tile is an infiniteFall tile.
+		boolean dl = level.getTile(x - 1, y + 1) == Tile.infiniteFall; //Checks if the lower-left tile is an infiniteFall tile.
+		boolean ur = level.getTile(x + 1, y - 1) == Tile.infiniteFall; //Checks if the upper-right tile is an infiniteFall tile.
+		boolean dr = level.getTile(x + 1, y + 1) == Tile.infiniteFall; //Checks if the lower-right tile is an infiniteFall tile.
 
-		if (!u && !l) {
-			if (!ul)
-				screen.render(x * 16 + 0, y * 16 + 0, 17, col, 0);
+	/* Commenter Note: All sentences with a "*" at the end means I'm making a guess, and not 100% sure. Please confirm it sometime in the future. */
+		
+		if (!u && !l) { // If there is no infiniteFall tile above or to the left of this...
+			if (!ul) // if there is no infiniteFall tile at the upper-left corner then...
+				screen.render(x * 16 + 0, y * 16 + 0, 17, col, 0); // render it as a normal flat cloud tile*
 			else
-				screen.render(x * 16 + 0, y * 16 + 0, 7 + 0 * 32, transitionColor, 3);
+				screen.render(x * 16 + 0, y * 16 + 0, 7 + 0 * 32, transitionColor, 3); // else render it as a corner piece*
 		} else
-			screen.render(x * 16 + 0, y * 16 + 0, (l ? 6 : 5) + (u ? 2 : 1) * 32, transitionColor, 3);
+			screen.render(x * 16 + 0, y * 16 + 0, (l ? 6 : 5) + (u ? 2 : 1) * 32, transitionColor, 3); // else have it render like a end peace*
 
-		if (!u && !r) {
-			if (!ur)
-				screen.render(x * 16 + 8, y * 16 + 0, 18, col, 0);
+		if (!u && !r) { // If there is no infiniteFall tile above or to the right of this...
+			if (!ur) // if there is no infiniteFall tile at the upper-right corner then...
+				screen.render(x * 16 + 8, y * 16 + 0, 18, col, 0); // render it as a normal flat cloud tile*
 			else
-				screen.render(x * 16 + 8, y * 16 + 0, 8 + 0 * 32, transitionColor, 3);
+				screen.render(x * 16 + 8, y * 16 + 0, 8 + 0 * 32, transitionColor, 3); // else render it as a corner piece*
 		} else
-			screen.render(x * 16 + 8, y * 16 + 0, (r ? 4 : 5) + (u ? 2 : 1) * 32, transitionColor, 3);
+			screen.render(x * 16 + 8, y * 16 + 0, (r ? 4 : 5) + (u ? 2 : 1) * 32, transitionColor, 3); // else have it render like a end peace*
 
-		if (!d && !l) {
-			if (!dl)
-				screen.render(x * 16 + 0, y * 16 + 8, 20, col, 0);
+		if (!d && !l) { // If there is no infiniteFall tile below or to the left of this...
+			if (!dl) // if there is no infiniteFall tile at the lower-left corner then...
+				screen.render(x * 16 + 0, y * 16 + 8, 20, col, 0); // render it as a normal flat cloud tile*
 			else
-				screen.render(x * 16 + 0, y * 16 + 8, 7 + 1 * 32, transitionColor, 3);
+				screen.render(x * 16 + 0, y * 16 + 8, 7 + 1 * 32, transitionColor, 3); // else render it as a corner piece*
 		} else
-			screen.render(x * 16 + 0, y * 16 + 8, (l ? 6 : 5) + (d ? 0 : 1) * 32, transitionColor, 3);
-		if (!d && !r) {
-			if (!dr)
-				screen.render(x * 16 + 8, y * 16 + 8, 19, col, 0);
+			screen.render(x * 16 + 0, y * 16 + 8, (l ? 6 : 5) + (d ? 0 : 1) * 32, transitionColor, 3); // else have it render like a end peace*
+		if (!d && !r) { // If there is no infiniteFall tile below or to the right of this...
+			if (!dr) // if there is no infiniteFall tile at the lower-right corner then...
+				screen.render(x * 16 + 8, y * 16 + 8, 19, col, 0); // render it as a normal flat cloud tile*
 			else
-				screen.render(x * 16 + 8, y * 16 + 8, 8 + 1 * 32, transitionColor, 3);
+				screen.render(x * 16 + 8, y * 16 + 8, 8 + 1 * 32, transitionColor, 3); // else render it as a corner piece*
 		} else
-			screen.render(x * 16 + 8, y * 16 + 8, (r ? 4 : 5) + (d ? 0 : 1) * 32, transitionColor, 3);
+			screen.render(x * 16 + 8, y * 16 + 8, (r ? 4 : 5) + (d ? 0 : 1) * 32, transitionColor, 3); // else have it render like a end peace*
 	}
 
+	/* Players can entities can walk on clouds */
 	public boolean mayPass(Level level, int x, int y, Entity e) {
 		return true;
 	}
 
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(5)) {
-					// level.setTile(xt, yt, Tile.infiniteFall, 0);
-					int count = random.nextInt(2) + 1;
-					for (int i = 0; i < count; i++) {
-						level.add(new ItemEntity(new ResourceItem(Resource.cloud), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
+		if (item instanceof ToolItem) { // If the player's current item is a tool...
+			ToolItem tool = (ToolItem) item; // Makes a ToolItem conversion of item.
+			if (tool.type == ToolType.shovel) { // If the tool is a shovel...
+				if (player.payStamina(5)) { //If the player can pay 5 stamina
+					int count = random.nextInt(2) + 1;// count is random from 0 to 1 and adds one. (1-2 count)
+					for (int i = 0; i < count; i++) { //cycles through the count
+						level.add(new ItemEntity(new ResourceItem(Resource.cloud), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3)); //adds a cloud item to the game
 					}
 					return true;
 				}
@@ -84,7 +91,4 @@ public class CloudTile extends Tile {
 		return false;
 	}
 
-	/*
-	 * public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) { if (item instanceof ToolItem) { ToolItem tool = (ToolItem) item; if (tool.type == ToolType.pickaxe) { if (player.payStamina(4 - tool.level)) { hurt(level, xt, yt, random.nextInt(10) + (tool.level) * 5 + 10); return true; } } } return false; }
-	 */
 }
--- src/com/mojang/ld22/level/tile/DirtTile.java
+++ src/com/mojang/ld22/level/tile/DirtTile.java
@@ -14,32 +14,32 @@ import com.mojang.ld22.sound.Sound;
 
 public class DirtTile extends Tile {
 	public DirtTile(int id) {
-		super(id);
+		super(id); //assigns the id
 	}
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int col = Color.get(level.dirtColor, level.dirtColor, level.dirtColor - 111, level.dirtColor - 111);
-		screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 0, 1, col, 0);
-		screen.render(x * 16 + 0, y * 16 + 8, 2, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0);
+		int col = Color.get(level.dirtColor, level.dirtColor, level.dirtColor - 111, level.dirtColor - 111); // Colors of the dirt (more info in level.java)
+		screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0); // renders the top-left part of the tile
+		screen.render(x * 16 + 8, y * 16 + 0, 1, col, 0); // renders the top-right part of the tile
+		screen.render(x * 16 + 0, y * 16 + 8, 2, col, 0); // renders the bottom-left part of the tile
+		screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0); // renders the bottom-right part of the tile
 	}
 
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.hole, 0);
-					level.add(new ItemEntity(new ResourceItem(Resource.dirt), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
-					Sound.monsterHurt.play();
+		if (item instanceof ToolItem) { // if the player's current item is a tool...
+			ToolItem tool = (ToolItem) item; // Makes a ToolItem conversion of item.
+			if (tool.type == ToolType.shovel) { // if the tool is a shovel...
+				if (player.payStamina(4 - tool.level)) { // if the player can pay the stamina...
+					level.setTile(xt, yt, Tile.hole, 0); //sets the tile to a hole
+					level.add(new ItemEntity(new ResourceItem(Resource.dirt), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3)); // pops out a dirt resource
+					Sound.monsterHurt.play();// sound plays
 					return true;
 				}
 			}
-			if (tool.type == ToolType.hoe) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.farmland, 0);
-					Sound.monsterHurt.play();
+			if (tool.type == ToolType.hoe) { // if the tool is a hoe...
+				if (player.payStamina(4 - tool.level)) { // if the player can pay the stamina...
+					level.setTile(xt, yt, Tile.farmland, 0); //sets the tile to a FarmTile
+					Sound.monsterHurt.play(); //sound plays
 					return true;
 				}
 			}

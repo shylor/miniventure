4cedd05bfbdd3d3e6eef5d70293c72e0d625ec0a
Davideesk <benepe20@gmail.com>
2013-07-19 19:58:19 -0500

    Commented on CactusTile.java & CloudCactusTile.java
    Trying to power through all the Tile files!

--- src/com/mojang/ld22/level/tile/CactusTile.java
+++ src/com/mojang/ld22/level/tile/CactusTile.java
@@ -13,43 +13,46 @@ import com.mojang.ld22.level.Level;
 
 public class CactusTile extends Tile {
 	public CactusTile(int id) {
-		super(id);
-		connectsToSand = true;
+		super(id); //Assigns the id
+		connectsToSand = true; // Can connect to sand
 	}
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int col = Color.get(20, 40, 50, level.sandColor);
-		screen.render(x * 16 + 0, y * 16 + 0, 8 + 2 * 32, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 0, 9 + 2 * 32, col, 0);
-		screen.render(x * 16 + 0, y * 16 + 8, 8 + 3 * 32, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 8, 9 + 3 * 32, col, 0);
+		int col = Color.get(20, 40, 50, level.sandColor); // colors of the cactus
+		screen.render(x * 16 + 0, y * 16 + 0, 8 + 2 * 32, col, 0); // renders the top-left part of the cactus
+		screen.render(x * 16 + 8, y * 16 + 0, 9 + 2 * 32, col, 0); // renders the top-right part of the cactus
+		screen.render(x * 16 + 0, y * 16 + 8, 8 + 3 * 32, col, 0); // renders the bottom-left part of the cactus
+		screen.render(x * 16 + 8, y * 16 + 8, 9 + 3 * 32, col, 0); // renders the bottom-right part of the cactus
 	}
 
+	/* Player cannot walk on the cactus (will act as a wall) */
 	public boolean mayPass(Level level, int x, int y, Entity e) {
 		return false;
 	}
 
+	/* Damage do to the cactus by the player */
 	public void hurt(Level level, int x, int y, Mob source, int dmg, int attackDir) {
-		int damage = level.getData(x, y) + dmg;
-		level.add(new SmashParticle(x * 16 + 8, y * 16 + 8));
-		level.add(new TextParticle("" + dmg, x * 16 + 8, y * 16 + 8, Color.get(-1, 500, 500, 500)));
-		if (damage >= 10) {
-			int count = random.nextInt(2) + 1;
-			for (int i = 0; i < count; i++) {
-				level.add(new ItemEntity(new ResourceItem(Resource.cactusFlower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3));
+		int damage = level.getData(x, y) + dmg; // Damage done to the cactus (it's "health" in some sense). dmg is the amount the player did to the cactus.
+		level.add(new SmashParticle(x * 16 + 8, y * 16 + 8)); // creates a smash particle
+		level.add(new TextParticle("" + dmg, x * 16 + 8, y * 16 + 8, Color.get(-1, 500, 500, 500))); // creates a text particle about how much damage has been done.
+		if (damage >= 10) { // If the damage is equal to, or larger than 10 then...
+			int count = random.nextInt(2) + 1; // count is random from 0 to 1 and adds one. (1-2 count)
+			for (int i = 0; i < count; i++) { //cycles through the count
+				level.add(new ItemEntity(new ResourceItem(Resource.cactusFlower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3));//adds a cactus flower
 			}
-			level.setTile(x, y, Tile.sand, 0);
+			level.setTile(x, y, Tile.sand, 0); // sets the tile to cactus
 		} else {
-			level.setData(x, y, damage);
+			level.setData(x, y, damage); // else it will set the data to damage
 		}
 	}
 
 	public void bumpedInto(Level level, int x, int y, Entity entity) {
-		entity.hurt(this, x, y, 1);
+		entity.hurt(this, x, y, 1); // the player will take 1 damage if they bump into it.
 	}
 
 	public void tick(Level level, int xt, int yt) {
-		int damage = level.getData(xt, yt);
-		if (damage > 0) level.setData(xt, yt, damage - 1);
+		int damage = level.getData(xt, yt); // gets the amount of damage the cactus has
+		if (damage > 0) level.setData(xt, yt, damage - 1); // If the number of damage is above 0, then it will minus itself by 1 (heal)
+		// Commenter note: I had no idea that cactuses healed themselves.
 	}
 }
--- src/com/mojang/ld22/level/tile/CloudCactusTile.java
+++ src/com/mojang/ld22/level/tile/CloudCactusTile.java
@@ -17,32 +17,33 @@ import com.mojang.ld22.level.Level;
 
 public class CloudCactusTile extends Tile {
 	public CloudCactusTile(int id) {
-		super(id);
-	}
+		super(id); //assigns the id
+	} 
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int color = Color.get(444, 111, 333, 555);
-		screen.render(x * 16 + 0, y * 16 + 0, 17 + 1 * 32, color, 0);
-		screen.render(x * 16 + 8, y * 16 + 0, 18 + 1 * 32, color, 0);
-		screen.render(x * 16 + 0, y * 16 + 8, 17 + 2 * 32, color, 0);
-		screen.render(x * 16 + 8, y * 16 + 8, 18 + 2 * 32, color, 0);
+		int color = Color.get(444, 111, 333, 555);  // colors of the cloud cactus
+		screen.render(x * 16 + 0, y * 16 + 0, 17 + 1 * 32, color, 0); // renders the top-left part of the cloud cactus
+		screen.render(x * 16 + 8, y * 16 + 0, 18 + 1 * 32, color, 0); // renders the top-right part of the cloud cactus
+		screen.render(x * 16 + 0, y * 16 + 8, 17 + 2 * 32, color, 0); // renders the bottom-left part of the cloud cactus
+		screen.render(x * 16 + 8, y * 16 + 8, 18 + 2 * 32, color, 0); // renders the bottom-right part of the cloud cactus
 	}
 
+	/* Determines what can pass this tile */
 	public boolean mayPass(Level level, int x, int y, Entity e) {
-		if (e instanceof AirWizard) return true;
+		if (e instanceof AirWizard) return true; // If the entity is the Air Wizard, then it can pass right through.
 		return false;
 	}
 
 	public void hurt(Level level, int x, int y, Mob source, int dmg, int attackDir) {
-		hurt(level, x, y, 0);
+		hurt(level, x, y, 0); // If you punch it, it will do 0 damage
 	}
 
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.pickaxe) {
-				if (player.payStamina(6 - tool.level)) {
-					hurt(level, xt, yt, 1);
+		if (item instanceof ToolItem) { // If the item the player is holding is a tool...
+			ToolItem tool = (ToolItem) item; // gets the tool from the item.
+			if (tool.type == ToolType.pickaxe) { // if the tool happens to be a pickaxe...
+				if (player.payStamina(6 - tool.level)) { // if the player can pay the stamina...
+					hurt(level, xt, yt, 1); // Do 1 damage to the cloud cactus (call the method below this one)
 					return true;
 				}
 			}
@@ -50,21 +51,22 @@ public class CloudCactusTile extends Tile {
 		return false;
 	}
 
+	/** This hurt method is special, called from the interact method above. */
 	public void hurt(Level level, int x, int y, int dmg) {
-		int damage = level.getData(x, y) + 1;
-		level.add(new SmashParticle(x * 16 + 8, y * 16 + 8));
-		level.add(new TextParticle("" + dmg, x * 16 + 8, y * 16 + 8, Color.get(-1, 500, 500, 500)));
-		if (dmg > 0) {
-			if (damage >= 10) {
-				level.setTile(x, y, Tile.cloud, 0);
+		int damage = level.getData(x, y) + 1; // Adds the damage to the tile's data
+		level.add(new SmashParticle(x * 16 + 8, y * 16 + 8)); // Adds a smash particle
+		level.add(new TextParticle("" + dmg, x * 16 + 8, y * 16 + 8, Color.get(-1, 500, 500, 500))); // Adds text of how much damage you've done.
+		if (dmg > 0) { //if the damage you did is over 0...
+			if (damage >= 10) { //if the current damage the cloud cactus has is equal to or larger than 0...
+				level.setTile(x, y, Tile.cloud, 0);// set the tile to cloud (destroys the cloud cactus tile)
 			} else {
-				level.setData(x, y, damage);
+				level.setData(x, y, damage);// else just do normal damage to it
 			}
 		}
 	}
 
 	public void bumpedInto(Level level, int x, int y, Entity entity) {
-		if (entity instanceof AirWizard) return;
-		entity.hurt(this, x, y, 3);
+		if (entity instanceof AirWizard) return; // The AirWizard will not get hurt by this
+		entity.hurt(this, x, y, 3); // 3 damage will be done to anyone who bumps into this (except the air-wizard)
 	}
 }

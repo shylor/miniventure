d7f22e7335992806f2776f2f11a36fcb50aaf907
Davideesk <benepe20@gmail.com>
2013-07-20 21:08:20 -0500

    Commented Sprite.java & SpriteSheet.java

--- src/com/mojang/ld22/gfx/Sprite.java
+++ src/com/mojang/ld22/gfx/Sprite.java
@@ -1,16 +1,16 @@
 package com.mojang.ld22.gfx;
 
 public class Sprite {
-	public int x, y;
-	public int img;
-	public int col;
-	public int bits;
+	public int x, y; // coordinates of the sprite
+	public int img; // image of the sprite
+	public int col; // color of the sprite
+	public int bits; // bits of the sprite
 
 	public Sprite(int x, int y, int img, int col, int bits) {
-		this.x = x;
-		this.y = y;
-		this.img = img;
-		this.col = col;
-		this.bits = bits;
+		this.x = x; // assigns the x coordinate
+		this.y = y; // assigns the y coordinate
+		this.img = img; // assigns the image value
+		this.col = col; // assigns the color value
+		this.bits = bits; // assigns the bits value
 	}
 }
--- src/com/mojang/ld22/gfx/SpriteSheet.java
+++ src/com/mojang/ld22/gfx/SpriteSheet.java
@@ -3,15 +3,16 @@ package com.mojang.ld22.gfx;
 import java.awt.image.BufferedImage;
 
 public class SpriteSheet {
-	public int width, height;
-	public int[] pixels;
+	public int width, height; // width and height of the sprite sheet
+	public int[] pixels; // the pixels on the image (integer array)
 
 	public SpriteSheet(BufferedImage image) {
-		width = image.getWidth();
-		height = image.getHeight();
-		pixels = image.getRGB(0, 0, width, height, null, 0, width);
-		for (int i = 0; i < pixels.length; i++) {
-			pixels[i] = (pixels[i] & 0xff) / 64;
+		width = image.getWidth(); // assigns the width from the image
+		height = image.getHeight(); // assigns the height from the image
+		pixels = image.getRGB(0, 0, width, height, null, 0, width); // Assigns the pixels of the image
+		for (int i = 0; i < pixels.length; i++) { //loops through all the pixels
+			/* The '& 0xff' part gets the last 8 bits of the 32-bit integer. */
+			pixels[i] = (pixels[i] & 0xff) / 64; //divides the last 8 bits of the pixel by 64. Doesn't seem to do much at all.
 		}
 	}
 }

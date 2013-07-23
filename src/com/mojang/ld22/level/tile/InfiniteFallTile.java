6581588d15663fe219d71208a31eaa960586e3b5
Davideesk <benepe20@gmail.com>
2013-07-23 15:18:09 -0500

    Commeneted InfiniteFallTile.java & StoneTile.java
    Two fast and easy classes

--- src/com/mojang/ld22/level/tile/InfiniteFallTile.java
+++ src/com/mojang/ld22/level/tile/InfiniteFallTile.java
@@ -6,18 +6,24 @@ import com.mojang.ld22.gfx.Screen;
 import com.mojang.ld22.level.Level;
 
 public class InfiniteFallTile extends Tile {
+	
+	/* This will be easy :D */
+	
 	public InfiniteFallTile(int id) {
-		super(id);
+		super(id); // assigns the id
 	}
 
+	/** Infinite fall tile doesn't render anything! */
 	public void render(Screen screen, Level level, int x, int y) {
 	}
 
+	/** Update method, updates (ticks) 60 times a second */
 	public void tick(Level level, int xt, int yt) {
 	}
 
+	/** Determines if an entity can pass through this tile */
 	public boolean mayPass(Level level, int x, int y, Entity e) {
-		if (e instanceof AirWizard) return true;
-		return false;
+		if (e instanceof AirWizard) return true; // If the entity is an Air Wizard, than it can pass through
+		return false; // else the entity can't pass through
 	}
 }
--- src/com/mojang/ld22/level/tile/StoneTile.java
+++ src/com/mojang/ld22/level/tile/StoneTile.java
@@ -7,21 +7,23 @@ import com.mojang.ld22.level.Level;
 
 public class StoneTile extends Tile {
 	public StoneTile(int id) {
-		super(id);
+		super(id); // assigns the id
 	}
 
+	/** Draws the tile to the screen */
 	public void render(Screen screen, Level level, int x, int y) {
-		int rc1 = 111;
-		int rc2 = 333;
-		int rc3 = 555;
-		screen.render(x * 16 + 0, y * 16 + 0, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0);
-		screen.render(x * 16 + 8, y * 16 + 0, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0);
-		screen.render(x * 16 + 0, y * 16 + 8, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0);
-		screen.render(x * 16 + 8, y * 16 + 8, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0);
+		int rc1 = 111; // first rock color
+		int rc2 = 333; // second rock color
+		int rc3 = 555; // third rock color
+		screen.render(x * 16 + 0, y * 16 + 0, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0); // renders the top-left part of the tile
+		screen.render(x * 16 + 8, y * 16 + 0, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0); // renders the top-right part of the tile
+		screen.render(x * 16 + 0, y * 16 + 8, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0); // renders the bottom-left part of the tile
+		screen.render(x * 16 + 8, y * 16 + 8, 32, Color.get(rc1, level.dirtColor, rc2, rc3), 0); // renders the bottom-right part of the tile
 	}
 
+	/** Determines if entities can pass through the tile */
 	public boolean mayPass(Level level, int x, int y, Entity e) {
-		return false;
+		return false; // they can't.
 	}
 
 }

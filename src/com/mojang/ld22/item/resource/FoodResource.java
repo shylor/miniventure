487bec91f1b2fc17b3b10bca1e77320fb70c0274
Davideesk <benepe20@gmail.com>
2013-07-22 22:11:19 -0500

    Commented FoodResource.java

--- src/com/mojang/ld22/item/resource/FoodResource.java
+++ src/com/mojang/ld22/item/resource/FoodResource.java
@@ -5,20 +5,21 @@ import com.mojang.ld22.level.Level;
 import com.mojang.ld22.level.tile.Tile;
 
 public class FoodResource extends Resource {
-	private int heal;
-	private int staminaCost;
+	private int heal; // the amount of health the food heals you for.
+	private int staminaCost; // the amount of stamina it costs to consume the food.
 
 	public FoodResource(String name, int sprite, int color, int heal, int staminaCost) {
-		super(name, sprite, color);
-		this.heal = heal;
-		this.staminaCost = staminaCost;
+		super(name, sprite, color); // assigns the name, sprite, and color
+		this.heal = heal; // assigns the heal amount
+		this.staminaCost = staminaCost; // assigns the stamina cost
 	}
 
+	/** What happens when the players uses the item on a tile */
 	public boolean interactOn(Tile tile, Level level, int xt, int yt, Player player, int attackDir) {
-		if (player.health < player.maxHealth && player.payStamina(staminaCost)) {
-			player.heal(heal);
-			return true;
+		if (player.health < player.maxHealth && player.payStamina(staminaCost)) { // If the player's health is less than the max health AND he can pay the stamina
+			player.heal(heal); // heal the player.
+			return true; // return true
 		}
-		return false;
+		return false; // return false
 	}
 }

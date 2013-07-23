203171c3be1f86700b9860b9da968408f02cb9a8
Davideesk <benepe20@gmail.com>
2013-07-23 15:12:04 -0500

    Commented FarmTile.java & FlowerTile.java

--- src/com/mojang/ld22/level/tile/FarmTile.java
+++ src/com/mojang/ld22/level/tile/FarmTile.java
@@ -11,23 +11,24 @@ import com.mojang.ld22.level.Level;
 
 public class FarmTile extends Tile {
 	public FarmTile(int id) {
-		super(id);
+		super(id); //assigns the id
 	}
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int col = Color.get(level.dirtColor - 121, level.dirtColor - 11, level.dirtColor, level.dirtColor + 111);
-		screen.render(x * 16 + 0, y * 16 + 0, 2 + 32, col, 1);
-		screen.render(x * 16 + 8, y * 16 + 0, 2 + 32, col, 0);
-		screen.render(x * 16 + 0, y * 16 + 8, 2 + 32, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 8, 2 + 32, col, 1);
+		int col = Color.get(level.dirtColor - 121, level.dirtColor - 11, level.dirtColor, level.dirtColor + 111); // gives the tile color based on level.dirtColor
+		screen.render(x * 16 + 0, y * 16 + 0, 2 + 32, col, 1); // renders the top-left tile
+		screen.render(x * 16 + 8, y * 16 + 0, 2 + 32, col, 0); // renders the top-right tile
+		screen.render(x * 16 + 0, y * 16 + 8, 2 + 32, col, 0); // renders bottom-left tile
+		screen.render(x * 16 + 8, y * 16 + 8, 2 + 32, col, 1); // renders the bottom-right
 	}
 
+	/* What happens when you use a tool on this tile */
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.dirt, 0);
+		if (item instanceof ToolItem) { // if the item happens to be a tool...
+			ToolItem tool = (ToolItem) item; // converts the Item object to a ToolItem object
+			if (tool.type == ToolType.shovel) { // if the type of the tool is a shovel...
+				if (player.payStamina(4 - tool.level)) { // If the player can pay the stamina...
+					level.setTile(xt, yt, Tile.dirt, 0); // sets the tile into a dirt tile
 					return true;
 				}
 			}
@@ -35,14 +36,19 @@ public class FarmTile extends Tile {
 		return false;
 	}
 
+	/** Update method, updates (ticks) 60 times a second */
 	public void tick(Level level, int xt, int yt) {
-		int age = level.getData(xt, yt);
-		if (age < 5) level.setData(xt, yt, age + 1);
+		int age = level.getData(xt, yt); // gets the current age of the tile
+		if (age < 5) level.setData(xt, yt, age + 1); // if the age is under 5, then adds 1 to the age
 	}
 
+	/** What happens when you step on the tile */
 	public void steppedOn(Level level, int xt, int yt, Entity entity) {
-		if (random.nextInt(60) != 0) return;
-		if (level.getData(xt, yt) < 5) return;
-		level.setTile(xt, yt, Tile.dirt, 0);
+		
+		if (random.nextInt(60) != 0) return; // if a random number between 0 and 59 does NOT equal 0, then skip the rest of this code
+		if (level.getData(xt, yt) < 5) return; // if the age of this tile is less than 5, then skip the rest of this code
+		//if (entity instanceof Player) // uncommented this bit if you only want the player to trample crops.
+		level.setTile(xt, yt, Tile.dirt, 0); // sets the tile to dirt.
+		
 	}
 }
--- src/com/mojang/ld22/level/tile/FlowerTile.java
+++ src/com/mojang/ld22/level/tile/FlowerTile.java
@@ -14,44 +14,47 @@ import com.mojang.ld22.level.Level;
 
 public class FlowerTile extends GrassTile {
 	public FlowerTile(int id) {
-		super(id);
-		tiles[id] = this;
-		connectsToGrass = true;
+		super(id); // this is here so that errors won't yell at us. Calls the GrassTile.java part of this class
+		tiles[id] = this; // assigns the id
+		connectsToGrass = true; // this tile can connect to grass.
 	}
 
+	/** Draws the tile on the screen */
 	public void render(Screen screen, Level level, int x, int y) {
-		super.render(screen, level, x, y);
+	super.render(screen, level, x, y); // calls the render method of GrassTile.java
 
-		int data = level.getData(x, y);
-		int shape = (data / 16) % 2;
-		int flowerCol = Color.get(10, level.grassColor, 555, 440);
+	int data = level.getData(x, y); // gets the data of this tile
+	int shape = (data / 16) % 2; // gets the shape of this tile. shape = the remainder of ((data/16) / 2)
+	int flowerCol = Color.get(10, level.grassColor, 555, 440); // color of the flower
 
-		if (shape == 0) screen.render(x * 16 + 0, y * 16 + 0, 1 + 1 * 32, flowerCol, 0);
-		if (shape == 1) screen.render(x * 16 + 8, y * 16 + 0, 1 + 1 * 32, flowerCol, 0);
-		if (shape == 1) screen.render(x * 16 + 0, y * 16 + 8, 1 + 1 * 32, flowerCol, 0);
-		if (shape == 0) screen.render(x * 16 + 8, y * 16 + 8, 1 + 1 * 32, flowerCol, 0);
+	if (shape == 0) screen.render(x * 16 + 0, y * 16 + 0, 1 + 1 * 32, flowerCol, 0); // if shape is equal to 0, then render it at the top-left part of the grass tile.
+	if (shape == 1) screen.render(x * 16 + 8, y * 16 + 0, 1 + 1 * 32, flowerCol, 0); // if shape is equal to 1, then render it at the top-right part of the grass tile.
+	if (shape == 1) screen.render(x * 16 + 0, y * 16 + 8, 1 + 1 * 32, flowerCol, 0); // if shape is equal to 1, then render it at the bottom-left part of the grass tile.
+	if (shape == 0) screen.render(x * 16 + 8, y * 16 + 8, 1 + 1 * 32, flowerCol, 0); // if shape is equal to 0, then render it at the bottom-right part of the grass tile.
 	}
 
+	/** What happens when you use an item on the tile */
 	public boolean interact(Level level, int x, int y, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(4 - tool.level)) {
-					level.add(new ItemEntity(new ResourceItem(Resource.flower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3));
-					level.add(new ItemEntity(new ResourceItem(Resource.flower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3));
-					level.setTile(x, y, Tile.grass, 0);
-					return true;
+		if (item instanceof ToolItem) { // if the item happens to be a tool...
+			ToolItem tool = (ToolItem) item; // converts the Item object into a ToolItem object
+			if (tool.type == ToolType.shovel) { // if the type of the tool is a shovel...
+				if (player.payStamina(4 - tool.level)) { // if the player can pay the stamina...
+				level.add(new ItemEntity(new ResourceItem(Resource.flower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3)); // adds a flower to the level
+				level.add(new ItemEntity(new ResourceItem(Resource.flower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3)); // adds a flowre to the level
+				level.setTile(x, y, Tile.grass, 0); // sets the tile to a grass tile
+				return true;
 				}
 			}
 		}
 		return false;
 	}
 
+	/** What happens when you punch the tile */
 	public void hurt(Level level, int x, int y, Mob source, int dmg, int attackDir) {
-		int count = random.nextInt(2) + 1;
-		for (int i = 0; i < count; i++) {
-			level.add(new ItemEntity(new ResourceItem(Resource.flower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3));
+		int count = random.nextInt(2) + 1; // random count between 1 and 2.
+		for (int i = 0; i < count; i++) { // cycles through the count
+			level.add(new ItemEntity(new ResourceItem(Resource.flower), x * 16 + random.nextInt(10) + 3, y * 16 + random.nextInt(10) + 3)); // adds a flower to the world
 		}
-		level.setTile(x, y, Tile.grass, 0);
+		level.setTile(x, y, Tile.grass, 0); // sets the tile to a grass tile
 	}
 }

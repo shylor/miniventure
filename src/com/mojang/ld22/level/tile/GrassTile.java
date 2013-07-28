ea8d19f3f8038f2ea6909f24c281a4a9a0f7bc23
Davideesk <benepe20@gmail.com>
2013-07-27 20:21:03 -0500

    Commented GrassTile.java & SandTile.java

--- src/com/mojang/ld22/level/tile/GrassTile.java
+++ src/com/mojang/ld22/level/tile/GrassTile.java
@@ -14,76 +14,80 @@ import com.mojang.ld22.sound.Sound;
 
 public class GrassTile extends Tile {
 	public GrassTile(int id) {
-		super(id);
-		connectsToGrass = true;
+		super(id); // assigns the id
+		connectsToGrass = true; // this tile can connect to grass tiles.
 	}
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int col = Color.get(level.grassColor, level.grassColor, level.grassColor + 111, level.grassColor + 111);
-		int transitionColor = Color.get(level.grassColor - 111, level.grassColor, level.grassColor + 111, level.dirtColor);
+		int col = Color.get(level.grassColor, level.grassColor, level.grassColor + 111, level.grassColor + 111); // the color of the grass
+		int transitionColor = Color.get(level.grassColor - 111, level.grassColor, level.grassColor + 111, level.dirtColor); // the transition color.
 
-		boolean u = !level.getTile(x, y - 1).connectsToGrass;
-		boolean d = !level.getTile(x, y + 1).connectsToGrass;
-		boolean l = !level.getTile(x - 1, y).connectsToGrass;
-		boolean r = !level.getTile(x + 1, y).connectsToGrass;
+		boolean u = !level.getTile(x, y - 1).connectsToGrass; // sees if the tile above this one can NOT connect to grass.
+		boolean d = !level.getTile(x, y + 1).connectsToGrass; // sees if the tile below this one can NOT connect to grass.
+		boolean l = !level.getTile(x - 1, y).connectsToGrass; // sees if the tile to the left of this one can NOT connect to grass.
+		boolean r = !level.getTile(x + 1, y).connectsToGrass; // sees if the tile to the right of this one can NOT connect to grass.
 
-		if (!u && !l) {
-			screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0);
+		if (!u && !l) { // if the tile above and the tile to the left can connect to grass then...
+			screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0); // renders a flat grass sprite in the upper-left corner of the sprite.
 		} else
-			screen.render(x * 16 + 0, y * 16 + 0, (l ? 11 : 12) + (u ? 0 : 1) * 32, transitionColor, 0);
+			screen.render(x * 16 + 0, y * 16 + 0, (l ? 11 : 12) + (u ? 0 : 1) * 32, transitionColor, 0); // else render a end piece.
 
-		if (!u && !r) {
-			screen.render(x * 16 + 8, y * 16 + 0, 1, col, 0);
+		if (!u && !r) {  // if the tile above and the tile to the right can connect to grass then...
+			screen.render(x * 16 + 8, y * 16 + 0, 1, col, 0); // renders a flat grass sprite in the upper-right corner of the sprite
 		} else
-			screen.render(x * 16 + 8, y * 16 + 0, (r ? 13 : 12) + (u ? 0 : 1) * 32, transitionColor, 0);
+			screen.render(x * 16 + 8, y * 16 + 0, (r ? 13 : 12) + (u ? 0 : 1) * 32, transitionColor, 0); // else render a end piece.
 
-		if (!d && !l) {
-			screen.render(x * 16 + 0, y * 16 + 8, 2, col, 0);
+		if (!d && !l) {  // if the tile below and the tile to the left can connect to grass then...
+			screen.render(x * 16 + 0, y * 16 + 8, 2, col, 0); // renders a flat grass sprite in the lower-left corner of the sprite
 		} else
-			screen.render(x * 16 + 0, y * 16 + 8, (l ? 11 : 12) + (d ? 2 : 1) * 32, transitionColor, 0);
-		if (!d && !r) {
-			screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0);
+			screen.render(x * 16 + 0, y * 16 + 8, (l ? 11 : 12) + (d ? 2 : 1) * 32, transitionColor, 0); // else render a end piece.
+		
+		if (!d && !r) {  // if the tile below and the tile to the right can connect to grass then...
+			screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0); // renders a flat grass sprite in the lower-right corner of the sprite
 		} else
-			screen.render(x * 16 + 8, y * 16 + 8, (r ? 13 : 12) + (d ? 2 : 1) * 32, transitionColor, 0);
+			screen.render(x * 16 + 8, y * 16 + 8, (r ? 13 : 12) + (d ? 2 : 1) * 32, transitionColor, 0); // else render a end piece.
 	}
 
+	/** Update method, updates (ticks) every 60 seconds. */
 	public void tick(Level level, int xt, int yt) {
-		if (random.nextInt(40) != 0) return;
+		int xn = xt; // next x position
+		int yn = yt; // next y position
 
-		int xn = xt;
-		int yn = yt;
-
-		if (random.nextBoolean())
-			xn += random.nextInt(2) * 2 - 1;
+		if (random.nextBoolean()) // makes a random decision of true or false
+			xn += random.nextInt(2) * 2 - 1; // if that decision is true, then the next x position = (random value between 0 to 1) * 2 - 1
 		else
-			yn += random.nextInt(2) * 2 - 1;
+			yn += random.nextInt(2) * 2 - 1; // if that decision is false, then the next y position = (random value between 0 to 1) * 2 - 1
 
-		if (level.getTile(xn, yn) == Tile.dirt) {
-			level.setTile(xn, yn, this, 0);
+		if (level.getTile(xn, yn) == Tile.dirt) { // if the next positions are a dirt tile then...
+			level.setTile(xn, yn, this, 0); // set that dirt tile to a grass tile
 		}
 	}
 
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.dirt, 0);
-					Sound.monsterHurt.play();
-					if (random.nextInt(5) == 0) {
+		if (item instanceof ToolItem) { // if the item happens to be a tool
+			ToolItem tool = (ToolItem) item; // converts the Item object into a ToolItem object.
+			
+			if (tool.type == ToolType.shovel) { // if the type of tool is a shovel...
+				if (player.payStamina(4 - tool.level)) { // if the player can pay the stamina...
+					level.setTile(xt, yt, Tile.dirt, 0); // sets the tile to a dirt tile
+					Sound.monsterHurt.play(); // plays a sound
+					if (random.nextInt(5) == 0) { // if a random value between 0 to 4 equals 0 then...
+						//Adds seeds to the world
 						level.add(new ItemEntity(new ResourceItem(Resource.seeds), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
 						return true;
 					}
 				}
 			}
-			if (tool.type == ToolType.hoe) {
-				if (player.payStamina(4 - tool.level)) {
-					Sound.monsterHurt.play();
-					if (random.nextInt(5) == 0) {
+			
+			if (tool.type == ToolType.hoe) { // if the type of tool is a hoe...
+				if (player.payStamina(4 - tool.level)) { // if the player can pay the stamina...
+					Sound.monsterHurt.play(); // plays a sound
+					if (random.nextInt(5) == 0) { // if a random value between 0 to 4 equals 0 then...
+						//Adds seeds to the world
 						level.add(new ItemEntity(new ResourceItem(Resource.seeds), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
-						return true;
+						return true; // skips the rest of the code
 					}
-					level.setTile(xt, yt, Tile.farmland, 0);
+					level.setTile(xt, yt, Tile.farmland, 0); // sets the tile to farmland
 					return true;
 				}
 			}
--- src/com/mojang/ld22/level/tile/SandTile.java
+++ src/com/mojang/ld22/level/tile/SandTile.java
@@ -15,65 +15,70 @@ import com.mojang.ld22.level.Level;
 
 public class SandTile extends Tile {
 	public SandTile(int id) {
-		super(id);
-		connectsToSand = true;
+		super(id); // assigns the id
+		connectsToSand = true; // this tile can connect to sand tiles.
 	}
 
 	public void render(Screen screen, Level level, int x, int y) {
-		int col = Color.get(level.sandColor + 2, level.sandColor, level.sandColor - 110, level.sandColor - 110);
-		int transitionColor = Color.get(level.sandColor - 110, level.sandColor, level.sandColor - 110, level.dirtColor);
+		int col = Color.get(level.sandColor + 2, level.sandColor, level.sandColor - 110, level.sandColor - 110); // the color of the grass
+		int transitionColor = Color.get(level.sandColor - 110, level.sandColor, level.sandColor - 110, level.dirtColor); // the transition color.
 
-		boolean u = !level.getTile(x, y - 1).connectsToSand;
-		boolean d = !level.getTile(x, y + 1).connectsToSand;
-		boolean l = !level.getTile(x - 1, y).connectsToSand;
-		boolean r = !level.getTile(x + 1, y).connectsToSand;
+		boolean u = !level.getTile(x, y - 1).connectsToSand; // sees if the tile above this one can NOT connect to sand.
+		boolean d = !level.getTile(x, y + 1).connectsToSand; // sees if the tile below this one can NOT connect to sand.
+		boolean l = !level.getTile(x - 1, y).connectsToSand; // sees if the tile to the left of this one can NOT connect to sand.
+		boolean r = !level.getTile(x + 1, y).connectsToSand; // sees if the tile to the right of this one can NOT connect to sand.
 
-		boolean steppedOn = level.getData(x, y) > 0;
+		boolean steppedOn = level.getData(x, y) > 0; // determines if the tile has been stepped on recently (for footprints)
 
-		if (!u && !l) {
-			if (!steppedOn)
-				screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0);
+		if (!u && !l) { // if the tile above and the tile to the left can connect to sand then...
+			if (!steppedOn) // if the sand has not been stepped on
+				screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0); // renders a flat sand sprite in the upper-left corner of the sprite.
 			else
-				screen.render(x * 16 + 0, y * 16 + 0, 3 + 1 * 32, col, 0);
+				screen.render(x * 16 + 0, y * 16 + 0, 3 + 1 * 32, col, 0); // renders a footprint instead.
 		} else
-			screen.render(x * 16 + 0, y * 16 + 0, (l ? 11 : 12) + (u ? 0 : 1) * 32, transitionColor, 0);
+			screen.render(x * 16 + 0, y * 16 + 0, (l ? 11 : 12) + (u ? 0 : 1) * 32, transitionColor, 0); // else render a end piece.
 
-		if (!u && !r) {
-			screen.render(x * 16 + 8, y * 16 + 0, 1, col, 0);
+		if (!u && !r) { // if the tile above and the tile to the right can connect to sand then...
+			screen.render(x * 16 + 8, y * 16 + 0, 1, col, 0); // renders a flat sand sprite in the upper-right corner of the sprite.
 		} else
-			screen.render(x * 16 + 8, y * 16 + 0, (r ? 13 : 12) + (u ? 0 : 1) * 32, transitionColor, 0);
-
-		if (!d && !l) {
-			screen.render(x * 16 + 0, y * 16 + 8, 2, col, 0);
+			screen.render(x * 16 + 8, y * 16 + 0, (r ? 13 : 12) + (u ? 0 : 1) * 32, transitionColor, 0); // else render a end piece.
+		
+		if (!d && !l) { // if the tile below and the tile to the left can connect to sand then...
+			screen.render(x * 16 + 0, y * 16 + 8, 2, col, 0); // renders a flat sand sprite in the lower-left corner of the sprite.
 		} else
-			screen.render(x * 16 + 0, y * 16 + 8, (l ? 11 : 12) + (d ? 2 : 1) * 32, transitionColor, 0);
-		if (!d && !r) {
+			screen.render(x * 16 + 0, y * 16 + 8, (l ? 11 : 12) + (d ? 2 : 1) * 32, transitionColor, 0); // else render a end piece.
+		
+		if (!d && !r) { // if the tile below and the tile to the right can connect to sand then...
 			if (!steppedOn)
-				screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0);
+				screen.render(x * 16 + 8, y * 16 + 8, 3, col, 0); // renders a flat sand sprite in the lower-right corner of the sprite.
 			else
-				screen.render(x * 16 + 8, y * 16 + 8, 3 + 1 * 32, col, 0);
+				screen.render(x * 16 + 8, y * 16 + 8, 3 + 1 * 32, col, 0); // renders a footprint instead.
 
 		} else
-			screen.render(x * 16 + 8, y * 16 + 8, (r ? 13 : 12) + (d ? 2 : 1) * 32, transitionColor, 0);
+			screen.render(x * 16 + 8, y * 16 + 8, (r ? 13 : 12) + (d ? 2 : 1) * 32, transitionColor, 0); // else render a end piece.
 	}
 
+	/** Update method, updates (ticks) every 60 seconds. */
 	public void tick(Level level, int x, int y) {
-		int d = level.getData(x, y);
-		if (d > 0) level.setData(x, y, d - 1);
+		int d = level.getData(x, y); // gets the current level data.
+		if (d > 0) level.setData(x, y, d - 1); // if that data is larger than 0, then decrease it by 1.
 	}
 
+	/** What happens when an entity steps on the tile */
 	public void steppedOn(Level level, int x, int y, Entity entity) {
-		if (entity instanceof Mob) {
-			level.setData(x, y, 10);
+		if (entity instanceof Mob) { // if the entity is a mob
+			level.setData(x, y, 10); // then set the data to 10. (footprints!)
 		}
 	}
 
+	/** What happens when you use an item in this tile */
 	public boolean interact(Level level, int xt, int yt, Player player, Item item, int attackDir) {
-		if (item instanceof ToolItem) {
-			ToolItem tool = (ToolItem) item;
-			if (tool.type == ToolType.shovel) {
-				if (player.payStamina(4 - tool.level)) {
-					level.setTile(xt, yt, Tile.dirt, 0);
+		if (item instanceof ToolItem) { // if the item happens to be a tool
+			ToolItem tool = (ToolItem) item; // converts the Item object into a ToolItem object.
+			if (tool.type == ToolType.shovel) { // if the type of tool is a shovel...
+				if (player.payStamina(4 - tool.level)) { // if the player can pay the stamina...
+					level.setTile(xt, yt, Tile.dirt, 0); // set the tile to a dirt tile
+					/* Adds a sand resource to the world */
 					level.add(new ItemEntity(new ResourceItem(Resource.sand), xt * 16 + random.nextInt(10) + 3, yt * 16 + random.nextInt(10) + 3));
 					return true;
 				}

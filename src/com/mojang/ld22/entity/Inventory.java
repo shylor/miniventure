de505ce00103029cd7a4c6cf80978d83c3fe22a2
Davideesk <benepe20@gmail.com>
2013-07-27 19:33:18 -0500

    Commented Color.java & Inventory.java

--- src/com/mojang/ld22/entity/Inventory.java
+++ src/com/mojang/ld22/entity/Inventory.java
@@ -8,61 +8,67 @@ import com.mojang.ld22.item.ResourceItem;
 import com.mojang.ld22.item.resource.Resource;
 
 public class Inventory {
-	public List<Item> items = new ArrayList<Item>();
+	public List<Item> items = new ArrayList<Item>(); // the list of items that is in the inventory.
 
+	/** Adds an item to the inventory */
 	public void add(Item item) {
-		add(items.size(), item);
+		add(items.size(), item); // adds a item into latest spot of the inventory
 	}
 
+	/** Adds an item to a specific spot in the inventory */
 	public void add(int slot, Item item) {
-		if (item instanceof ResourceItem) {
-			ResourceItem toTake = (ResourceItem) item;
-			ResourceItem has = findResource(toTake.resource);
-			if (has == null) {
-				items.add(slot, toTake);
+		if (item instanceof ResourceItem) {// if the item happens to be a resource
+			ResourceItem toTake = (ResourceItem) item; // converts the item object into a ResourceItem object
+			ResourceItem has = findResource(toTake.resource); // finds if you have the resource in their inventory
+			if (has == null) { // if the owner of this inventory doesn't have the resource
+				items.add(slot, toTake); // add the resource in the items list
 			} else {
-				has.count += toTake.count;
+				has.count += toTake.count; // else add to the count of the item resource
 			}
 		} else {
-			items.add(slot, item);
+			items.add(slot, item); // add the item to the items list
 		}
 	}
 
+	/** Finds a resource in your inventory */
 	private ResourceItem findResource(Resource resource) {
-		for (int i = 0; i < items.size(); i++) {
-			if (items.get(i) instanceof ResourceItem) {
-				ResourceItem has = (ResourceItem) items.get(i);
-				if (has.resource == resource) return has;
+		for (int i = 0; i < items.size(); i++) { // loops through the items list
+			if (items.get(i) instanceof ResourceItem) { // if the current item is a ResourceItem
+				ResourceItem has = (ResourceItem) items.get(i); // converts the item object into a ResourceItem object
+				if (has.resource == resource) return has; // returns if the loop has found a matching resource in your inventory
 			}
 		}
-		return null;
+		return null; // else it will return null
 	}
 
+	/** Returns true if the player has the resource, and has equal or more than the amount given */
 	public boolean hasResources(Resource r, int count) {
-		ResourceItem ri = findResource(r);
-		if (ri == null) return false;
-		return ri.count >= count;
+		ResourceItem ri = findResource(r); // finds the resource
+		if (ri == null) return false; // if the player doesn't have the resource, then return false
+		return ri.count >= count; // if the player has more or equal amount of resource than the count, then return true.
 	}
 
+	/** Removes resources from your inventory */
 	public boolean removeResource(Resource r, int count) {
-		ResourceItem ri = findResource(r);
-		if (ri == null) return false;
-		if (ri.count < count) return false;
-		ri.count -= count;
-		if (ri.count <= 0) items.remove(ri);
+		ResourceItem ri = findResource(r); // finds the resource
+		if (ri == null) return false; // if the resource cannot be found, then skip the rest of the code
+		if (ri.count < count) return false; // if the resource amount is smaller than the count, then skip the rest of the code.
+		ri.count -= count; // minus the count.
+		if (ri.count <= 0) items.remove(ri); // if the count is smaller than or equal to 0 then remove the resource from the list
 		return true;
 	}
 
+	/** Returns the amount of an item you have in the inventory */
 	public int count(Item item) {
-		if (item instanceof ResourceItem) {
-			ResourceItem ri = findResource(((ResourceItem)item).resource);
-			if (ri!=null) return ri.count;
-		} else {
-			int count = 0;
-			for (int i=0; i<items.size(); i++) {
-				if (items.get(i).matches(item)) count++;
+		if (item instanceof ResourceItem) { // if the item is a resource...
+			ResourceItem ri = findResource(((ResourceItem)item).resource); // finds the resource
+			if (ri!=null) return ri.count; // returns the amount of that resource
+		} else { // if the item is NOT a resource...
+			int count = 0; // the count of the item
+			for (int i=0; i<items.size(); i++) { // loops through the player's inventory
+				if (items.get(i).matches(item)) count++; // if the current item matches the one given, then the count increases.
 			}
-			return count;
+			return count; // returns the count.
 		}
 		return 0;
 	}
--- src/com/mojang/ld22/gfx/Color.java
+++ src/com/mojang/ld22/gfx/Color.java
@@ -2,16 +2,30 @@ package com.mojang.ld22.gfx;
 
 public class Color {
 
+	
+	/* To explain this class, you have to know what a int (integer) is in Java and how Bit-Shifting works.
+	I made a small post, so go here if you don't already know: http://minicraftforums.com/viewtopic.php?f=9&t=2256
+	*/
+	
+	/* Note: this class still confuses me a bit, lol. -David */
+	
+	/** This returns a integer with 4 rgb color values. */
 	public static int get(int a, int b, int c, int d) {
-		return (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a));
+		return (get(d) << 24) // gets the d value, and shifts its bit's 24 times to the left and adds...
+		+ (get(c) << 16) // gets the c value, and shifts its bit's 16 times to the left and adds...
+		+ (get(b) << 8) // gets the b value, and shifts its bit's 8 times to the left and adds...
+		+ (get(a)); // gets the a value.
 	}
 
+	/** gets the color to use based off an integer */
 	public static int get(int d) {
-		if (d < 0) return 255;
-		int r = d / 100 % 10;
-		int g = d / 10 % 10;
-		int b = d % 10;
-		return r * 36 + g * 6 + b;
+		if (d < 0) return 255; // if d is smaller than 0, then return 255.
+		int r = d / 100 % 10; // the red value is the remainder of (d/100) / 10
+		int g = d / 10 % 10; // the green value is the remainder of (d/10) / 10
+		int b = d % 10; // the blue value is the remainder of d / 10.
+		return r * 36 + g * 6 + b; // returns (red value * 36) + (green value * 6) + (blue value)
+		
+		// Why do we need all this math to get the colors? I don't even know. -David
 	}
 
 }
